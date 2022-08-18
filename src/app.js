8fa94b3cccbb76a3b036c83869412c1c3b4ba4e7
joshacross <joshuaalexandercross@gmail.com>
2022-08-18 16:24:26 -0500

    resolved import challenge, created Card component

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Header from './components/Header';
-import FeedbackList from './components/FeedbackItem';
+import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 
 function App() {
--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,22 +1,17 @@
-import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Card from './shared/Card';
 
-function FeedbackItem() {
-     const [rating, setRating] = useState(7);
-     const [text, setText] = useState('This is an example of a feedback item');
-
-     const handleClick = () => {
-          setRating((prev) => {
-               return prev + 1;
-          });
-     };
-
+function FeedbackItem({ item }) {
      return (
-          <div className='card'>
-               <div className='num-display'>{rating}</div>
-               <div className='text-display'>{text}</div>
-               <button onClick={handleClick}>Click Me</button>
-          </div>
+          <Card>
+               <div className='num-display'>{item.rating}</div>
+               <div className='text-display'>{item.text}</div>
+          </Card>
      );
 }
+
+FeedbackItem.propTypes = {
+     item: PropTypes.object.isRequired,
+};
 
 export default FeedbackItem;
